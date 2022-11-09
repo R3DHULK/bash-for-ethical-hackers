f8406671292638cf79eda94a451a02041584eaf2
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-09 18:34:21 +0530

    Add files via upload

---
+++ admin-panel-finder.sh
@@ -0,0 +1,25 @@
+clear
+
+cd is-this-your-admin
+
+clear
+
+git clone https://github.com/R3DHULK/is-this-your-admin/
+
+clear
+
+cd is-this-your-admin
+
+clear
+
+sudo apt install python
+
+clear
+
+python -m pip install -r requirements.txt
+
+clear
+
+python admin-panel-finder.py
+
+clear
