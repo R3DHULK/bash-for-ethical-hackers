713efaafc3a968f975c7bb6f7d18633093a5aa26
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-09 23:54:20 +0530

    fakemail

---
+++ fakemail.sh
@@ -0,0 +1,19 @@
+clear
+
+git clone https://github.com/R3DHULK/fakemail
+
+clear
+
+cd fakemail
+
+clear
+
+apt install python
+
+clear
+
+python -m pip install -r requirements.txt
+
+clear
+
+python fakemail.py
