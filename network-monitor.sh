0a380dec81c706619bc409a705a0d4e63ab9d461
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-09 23:55:39 +0530

    check net status

---
+++ network-monitor.sh
@@ -0,0 +1,17 @@
+clear
+
+apt install git
+
+git clone https://github.com/R3DHULK/all-about-network
+
+cd all-about-network
+
+apt install python
+
+clear
+
+python -m pip install -r requirements.txt
+
+clear
+
+python network-monitoring.py
