6a8640fd1849cb6106ae7ad1ee2a35cce2e6412f
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-09 20:35:25 +0530

    Add files via upload

---
+++ arp-spoofing-detect.sh
@@ -0,0 +1,20 @@
+clear
+
+git clone https://github.com/R3DHULK/mission-arp
+
+clear
+
+cd mission-arp
+
+clear
+
+sudo apt install python
+
+clear
+
+python -m pip install -r requirements.txt
+
+clear
+
+python arp-spoofing-detect.py
+
