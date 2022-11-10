721c4f1f2c561fc46c962eed6655a6e091ee52e1
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-11-10 23:37:21 +0530

    Add files via upload

---
+++ shodan-api.sh
@@ -0,0 +1,19 @@
+clear
+
+apt install git
+
+clear
+
+git clone https://github.com/R3DHULK/utilise_SHODAN-API
+
+clear 
+
+cd utilise_SHODAN-API
+
+clear
+
+apt install python
+
+clear
+
+python shodan_api.py
